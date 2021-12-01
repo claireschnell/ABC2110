880999d24b92caf0c9e81e28eb5b45f57114786c
claireschnell <schnellclairefrance@gmail.com>
2021-12-01 11:47:09 +0100

    CS Ajout exo moyenne

---
+++ eclipse-workspace/Exercices/src/MainClass.java
@@ -0,0 +1,21 @@
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Lire le rayon R d'une sphère
+		//DECLARATION DES VARIABLES
+		
+		//réel air 4  R2;
+		float air 4  R2;
+		//réel volume 4/3  R3;
+		float 4/3  R3;
+		
+		//SAISIES
+		//écrire " air 4  R2"  "volume 4/3  R3 ");
+		System.out.println(" air 4  R2");
+		System.out.printIn("volume 4/3  R3 ");
+	
+	}
+
+}
